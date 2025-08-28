f35beb6c6db982ad5fea7f0b87ee71fa013452ec
Syafiqah Azmee <115607602+syafiqahazmee216427@users.noreply.github.com>
2025-08-29 01:24:34 +0800

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,6 +3,4 @@ import networkx as nx
 # Task 1.2: Choose and Load one ego-network into NetworkX
 # Chosen ego-network: the edge list for ego user 0
 # Load
-def load_graph(path):
-    G = nx.read_edgelist(path)
-    return G
+G = nx.read_edgelist("facebook/0.edges")
